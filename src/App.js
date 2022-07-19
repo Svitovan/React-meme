ad0142e95761d7279a250cbf09ba7f17f15922d6
WorkPC <svitovan@outlook.com>
2022-07-19 16:29:26 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header';
+import Meme from './Components/Meme';
+// import data from './data';
+
+export default function App() {
+  
+  return (
+    
+     <div>
+        <Header />
+        <Meme />
+     </div>
+  )
+}
