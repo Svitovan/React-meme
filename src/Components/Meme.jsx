bdc3e1e2a5790d073554e13802c2467966b6ba9a
WorkPC <svitovan@outlook.com>
2023-01-12 11:39:55 +0100

    randomImage

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -7,7 +7,7 @@ export default function Meme() {
     const [meme, setMeme] = React.useState({
         topText: '',
         bottomText: '', 
-        randomImage: 'http://i.imgflip.com/1bij.jpg'
+        randomImage: 'https://picsum.photos/500'
     })
     const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
